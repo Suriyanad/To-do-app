8acbb6c7c766aa234c4d8421ef0c55bef4f62862
Suriyanad <106733060+Suriyanad@users.noreply.github.com>
2022-11-10 22:06:30 +0530

    Update InputField.tsx

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import "./InputField.scss";
 
-// Set interface to define type for props
+
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
@@ -9,14 +9,12 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAddTodo }) => {
-  // useRef hook for input field focus
   const inputRef = useRef<HTMLInputElement>(null);
 
 
   return (
     <form className="input" onSubmit={(e) => {
       handleAddTodo(e)
-      // Shifts focus from the input box, so page shadow disappears
       inputRef.current?.blur();
     }}>
       <input
